fa8e3698bdbe5ec726bc0b4cd7ec6b1d4f885248
Yuri Karmakov <unoegohh@gmail.com>
2013-06-02 19:45:03 +0400

    composer

--- src/Unoegohh/EbayBundle/Controller/DefaultController.php
+++ src/Unoegohh/EbayBundle/Controller/DefaultController.php
@@ -14,24 +14,6 @@ class DefaultController extends Controller
     public function indexAction()
     {
 
-        $outputSelector = 'PictureURLSuperSize';
-
-        $url = "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced" .
-            "&SERVICE-VERSION=1.11.0" .
-            "&SECURITY-APPNAME=DandMe38d-69bb-4a41-aeb2-159622d4095" .
-            "&RESPONSE-DATA-FORMAT=XML" .
-            "&REST-PAYLOAD" .
-            "&itemFilter(0).name=Seller" .
-            "&itemFilter(0).value=loose.diamonds.king" .
-            "&itemFilter(1).name=LocatedIn" .
-            "&itemFilter(1).value=WorldWide".
-//            "&paginationInput.entriesPerPage=15&paginationInput.pageNumber=1".
-            "&outputSelector=$outputSelector"
-            ;
-
-        $find = file_get_contents($url);
-        $result = new SimpleXMLElement($find);
-
 
         /**
          * @var $em EntityManager
@@ -42,54 +24,83 @@ class DefaultController extends Controller
 
         $connection->executeUpdate($platform->getTruncateTableSQL('RawItem', true /* whether to cascade */));
 
+        $currentPage = 0;
         $count = 0;
         $newItemsCount = 0;
-        $output = array();
-        $titles = array();
-        foreach($result->searchResult->item as $item){
+        $k = 1;
+        while($currentPage == 0){
+
+            $outputSelector = 'PictureURLSuperSize';
+
+            $url = "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced" .
+                "&SERVICE-VERSION=1.11.0" .
+                "&SECURITY-APPNAME=DandMe38d-69bb-4a41-aeb2-159622d4095" .
+                "&RESPONSE-DATA-FORMAT=XML" .
+                "&REST-PAYLOAD" .
+                "&itemFilter(0).name=Seller" .
+                "&itemFilter(0).value=loose.diamonds.king" .
+                "&itemFilter(1).name=LocatedIn" .
+                "&itemFilter(1).value=WorldWide".
+                "&paginationInput.entriesPerPage=100&paginationInput.pageNumber=$k".
+                "&outputSelector=$outputSelector"
+                ;
+
+            $find = file_get_contents($url);
+            $result = new SimpleXMLElement($find);
+
+            $k++;
 
 
-            $newItem = new RawItem();
-            $newItem->setName((string)$item->title);
-            $newItem->setIName(preg_replace('/([0-9.]+ ?)+/', '[] ', $item->title));
-            $newItem->setItemId($item->itemId);
-            $newItem->setImage($item->pictureURLSuperSize);
-            $newItem->setPrice($item->sellingStatus->currentPrice);
-
-
-            $img_url = '/uploads/'. md5($item->pictureURLSuperSize) . '.png';
-            $newItem->setLocalImg($img_url);
-            $em->persist($newItem);
-            $em->flush();
-            $count++;
-
-
-            $search = array();
-            $transItem = new TranslationItem();
-
-            $ch = curl_init($item->pictureURLSuperSize);
-            $fp = fopen($this->get('kernel')->getRootDir() . '/../web' . $img_url, 'ab+');
-            curl_setopt($ch, CURLOPT_FILE, $fp);
-            curl_setopt($ch, CURLOPT_HEADER, 0);
-            curl_exec($ch);
-            curl_close($ch);
-            fclose($fp);
-
-            $search['engText'] = $newItem->getIName();
-            $result = $em->getRepository('UnoegohhEbayBundle:TranslationItem')->findBy($search);
-
-
-
-            if(!count($result)){
-                $transItem->setEngText($newItem->getIName());
-                $transItem->setTranslated(false);
-                $transItem->setCurrTrans(false);
-                $em->persist($transItem);
-                $em->flush();
-                $newItemsCount++;
+            $output = array();
+            $titles = array();
+            if(count($result->searchResult->item) < 100){
+                $currentPage = 1;
             }
-        }
+            foreach($result->searchResult->item as $item){
 
+
+                $newItem = new RawItem();
+                $newItem->setName((string)$item->title);
+                $newItem->setIName(preg_replace('/([0-9.]+ ?)+/', '[] ', $item->title));
+                $newItem->setItemId($item->itemId);
+                $newItem->setImage($item->pictureURLSuperSize);
+                $newItem->setPrice($item->sellingStatus->currentPrice);
+
+
+                $img_url = '/uploads/'. md5($item->pictureURLSuperSize) . '.png';
+                $newItem->setLocalImg($img_url);
+                $em->persist($newItem);
+                $em->flush();
+                $count++;
+
+
+                $search = array();
+                $transItem = new TranslationItem();
+
+                $ch = curl_init($item->pictureURLSuperSize);
+                $fp = fopen($this->get('kernel')->getRootDir() . '/../web' . $img_url, 'ab+');
+                curl_setopt($ch, CURLOPT_FILE, $fp);
+                curl_setopt($ch, CURLOPT_HEADER, 0);
+                curl_exec($ch);
+                curl_close($ch);
+                fclose($fp);
+
+                $search['engText'] = $newItem->getIName();
+                $result = $em->getRepository('UnoegohhEbayBundle:TranslationItem')->findBy($search);
+
+
+
+                if(!count($result)){
+                    $transItem->setEngText($newItem->getIName());
+                    $transItem->setTranslated(false);
+                    $transItem->setCurrTrans(false);
+                    $em->persist($transItem);
+                    $em->flush();
+                    $newItemsCount++;
+                }
+            }
+
+        }
         $response = new Response('Total parsed ' . $count . '<br>' . 'Added new titles ' . $newItemsCount);
         return $response;
     }

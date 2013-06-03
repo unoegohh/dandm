05e92936d4ed4ab0660172e5bf540d5f4dee0378
Yuri Karmakov <unoegohh@gmail.com>
2013-06-03 16:31:11 +0400

    heder links fix

--- src/Unoegohh/EbayBundle/Controller/DefaultController.php
+++ src/Unoegohh/EbayBundle/Controller/DefaultController.php
@@ -23,6 +23,7 @@ class DefaultController extends Controller
         $platform   = $connection->getDatabasePlatform();
 
         $connection->executeUpdate($platform->getTruncateTableSQL('RawItem', true /* whether to cascade */));
+        $connection->executeUpdate($platform->getTruncateTableSQL('TranslationItem', true /* whether to cascade */));
 
         $currentPage = 0;
         $count = 0;
